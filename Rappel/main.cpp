0cce84a8296939ac947410c44c567489647d0ebc
EvanGulmez <94354080+EvanGulmez@users.noreply.github.com>
2021-11-15 12:05:47 +0100

    Prix a deviner

--- Rappel/main.cpp
+++ Rappel/main.cpp
@@ -4,6 +4,20 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+   /*cout << "Hello world!" << endl;
+    return 0;*/
+    printf("Devine le prix\n");
+
+    auto aDeviner(158);
+    auto SaisiaDeviner(0);
+    cin>> SaisiaDeviner;
+
+   // cout<< aDeviner;
+
+    if(SaisiaDeviner != aDeviner ){
+
+       printf("Ce n'est pas le bon prix");
+
+
+    }
 }
