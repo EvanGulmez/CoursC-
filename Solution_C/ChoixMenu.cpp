1aa27d0e01e259c00bd6c6a6a212367c58fc645c
EvanGulmez <94354080+EvanGulmez@users.noreply.github.com>
2021-11-16 09:44:51 +0100

    Juste prix avec constante.h

---
+++ Solution_C/ChoixMenu.cpp
@@ -0,0 +1,3 @@
+#include "constante.h"
+#include <iostream>
+using namespace std;
--- Solution_C/constante.h
+++ Solution_C/constante.h
@@ -5,5 +5,4 @@ const auto BORNE_MIN{0};
 const auto BORNE_MAX{10'000};
 const auto BORNE_MAX_FACILE{1'000};
 
-
 #endif // CONSTANTE_H_INCLUDED
