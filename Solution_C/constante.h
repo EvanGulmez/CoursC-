42344567f65fd041d11dd6a7f37d3e4875d20685
EvanGulmez <94354080+EvanGulmez@users.noreply.github.com>
2021-11-16 09:38:41 +0100

    Juste prix avec constante .h

---
+++ Solution_C/constante.h
@@ -0,0 +1,9 @@
+#ifndef CONSTANTE_H_INCLUDED
+#define CONSTANTE_H_INCLUDED
+
+const auto BORNE_MIN{0};
+const auto BORNE_MAX{10'000};
+const auto BORNE_MAX_FACILE{1'000};
+
+
+#endif // CONSTANTE_H_INCLUDED
